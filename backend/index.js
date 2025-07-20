d8e94023783f58d2a26ef6027de685463ca1a240
siddsingh06 <sidd060905@gmail.com>
2025-07-20 21:27:47 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,27 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-// DB Connect    
+
 connectToMongo();
 
-// Middlewares
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://notehere.onrender.com", // frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes
+
 app.use("/api/notes", require("./routes/notes"));
 app.use("/api/auth", require("./routes/auth"));
-// Start server
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
